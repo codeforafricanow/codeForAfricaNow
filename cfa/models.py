ca22b0b7290f62428ba1520c433195956f5eb383
codeforafricanow <codeforafricanow@gmail.com>
2024-02-20 23:48:03 +0300

    changed img type

--- cfa/models.py
+++ cfa/models.py
@@ -44,7 +44,7 @@ class Content(models.Model):
    
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png',null=True, blank=True, upload_to='profile_pics')
+    image = models.ImageField(default='default.PNG',null=True, blank=True, upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
